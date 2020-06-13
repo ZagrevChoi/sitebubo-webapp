7f6fabde1af138cd62a22e831ec07ce220b935ce
ZagrevChoi <sitebubo@gmail.com>
2020-06-13 14:22:27 +0300

    add: items for styling the minimized cards

--- src/app/dashboard/dashboard.module.ts
+++ src/app/dashboard/dashboard.module.ts
@@ -1,14 +1,17 @@
 import { ButtonsModule } from './../shared/components/buttons/buttons.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { NgSelectModule } from '@ng-select/ng-select';
+import { FormsModule } from '@angular/forms';
+
 
 import { DashboardRoutingModule } from './dashboard-routing.module';
 import { DashboardComponent } from './dashboard.component';
 
-
-import { DragDropModule } from '@angular/cdk/drag-drop';
 import { ExpandedModule } from '../shared/components/dashboard/expanded/expanded.module';
 import { MinimizedModule } from '../shared/components/dashboard/minimized/minimized.module';
+import { DragDropModule } from '@angular/cdk/drag-drop';
+
 
 @NgModule({
   declarations: [
@@ -18,9 +21,11 @@ import { MinimizedModule } from '../shared/components/dashboard/minimized/minimi
     CommonModule,
     DashboardRoutingModule,
     ButtonsModule,
-    DragDropModule,
     ExpandedModule,
-    MinimizedModule
+    MinimizedModule,
+    NgSelectModule,
+    FormsModule,
+    DragDropModule
   ]
 })
 export class DashboardModule { }
