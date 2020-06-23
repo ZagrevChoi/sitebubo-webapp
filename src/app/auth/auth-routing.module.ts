7824cdf31eea51fd6a204b6d4d16028a8e955378
ZagrevChoi <sitebubo@gmail.com>
2020-06-23 15:08:05 +0300

    add: layout for authentication page

--- src/app/auth/auth-routing.module.ts
+++ src/app/auth/auth-routing.module.ts
@@ -2,14 +2,33 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { LoginComponent } from './login/login.component';
+import { SignupComponent } from './signup/signup.component';
+import { BrandComponent } from './brand/brand.component';
 
 const routes: Routes = [
   {
-    path: '', redirectTo: 'login', pathMatch: 'full'
+    path: '', component: BrandComponent,
+    children: [
+      {
+        path: '', redirectTo: 'login', pathMatch: 'full'
+      },
+      {
+        path: 'login', component: LoginComponent
+      },
+      {
+        path: 'signup', component: SignupComponent
+      } 
+    ]
   },
-  {
-    path: 'login', component: LoginComponent
-  }
+  // {
+  //   path: '', redirectTo: 'login', pathMatch: 'full'
+  // },
+  // {
+  //   path: 'login', component: LoginComponent
+  // },
+  // {
+  //   path: 'signup', component: SignupComponent
+  // }
 ];
 
 @NgModule({
